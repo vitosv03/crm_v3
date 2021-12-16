ce9388835c96cf7329f31e60e6dfed43b6272b68
vitosv03 <vitosv03@gmail.com>
2021-12-16 22:55:05 +0200

    add sort_title to filters.py

--- crm-project/Apps/crm_app/filters.py
+++ crm-project/Apps/crm_app/filters.py
@@ -1,14 +1,24 @@
+from random import choices
+
 import django_filters
 
 from .models import ClientsInfo
 
 
 class ClientsInfoFilter(django_filters.FilterSet):
+    CHOICES = (
+        ('asc', 'asc'),
+        ('desc', 'desc'),
+    )
+
+    ordering = django_filters.ChoiceFilter(label='Ordering', choices=CHOICES, method='filter_by_order')
 
     class Meta:
         model = ClientsInfo
         fields = {
             'title': ['icontains'],
-
         }
 
+    def filter_by_order(self, queryset, name, value):
+        title_sort = 'title' if value == 'asc' else '-title'
+        return queryset.order_by(title_sort)
