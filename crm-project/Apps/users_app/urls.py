c7e894ea37b0c2d14f5e8e306b495d7fe59d2eb0
vitosv03 <vitosv03@gmail.com>
2021-12-21 20:49:56 +0200

    my profile can see only own records

--- crm-project/Apps/users_app/urls.py
+++ crm-project/Apps/users_app/urls.py
@@ -11,7 +11,11 @@ urlpatterns = [
     path('user/', include([
         path('list/', views.UsersListView.as_view(), name='user_list'),
         path('<int:pk>/detail/', views.UserDetailView.as_view(), name='user_detail'),
+        # path('detail/', views.UserDetailView.as_view(), name='user_detail'),
+        # path('update/', views.UserUpdateView.as_view(), name='user_update'),
         path('<int:pk>/update/', views.UserUpdateView.as_view(), name='user_update'),
+
+
         # path('add/', tag_views.TagAddView.as_view(), name='tag_add'),
         # path('<int:pk>/detail/', tag_views.TagDetailView.as_view(), name='tag_detail'),
         # path('<int:pk>/update/', tag_views.TagUpdateView.as_view(), name='tag_update'),
--- crm-project/Apps/users_app/views.py
+++ crm-project/Apps/users_app/views.py
@@ -1,13 +1,15 @@
 from django.contrib.auth import logout
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, UpdateView
 
 from .models import Users
 from django.contrib.auth.views import LoginView
 from .forms import LoginUserForm
+
+
 # Create your views here.
 
 def usersHome(request):
@@ -17,12 +19,12 @@ def usersHome(request):
 
 def home(request):
     current_user = request.user
-    return render(request, 'home.html', dict(user=current_user) )
+    return render(request, 'home.html', dict(user=current_user))
 
 
 def listUsers(request):
     all_users = Users.objects.all()
-    return render(request,'list_users.html', dict(all_users=all_users))
+    return render(request, 'list_users.html', dict(all_users=all_users))
 
 
 class LoginUserView(LoginView):
@@ -48,6 +50,7 @@ class UsersListView(LoginRequiredMixin, ListView):
     model = Users
     template_name = 'users_list.html'
     context_object_name = 'users'
+
     # permission_required = 'crm_app.view_tags'
 
     def get_context_data(self, **kwargs):
@@ -61,6 +64,7 @@ class UserDetailView(LoginRequiredMixin, DetailView):
     model = Users
     template_name = 'user_detail.html'
     context_object_name = 'user'
+
     # permission_required = 'crm_app.view_tags'
 
     def get_context_data(self, **kwargs):
@@ -68,28 +72,31 @@ class UserDetailView(LoginRequiredMixin, DetailView):
         context['title'] = 'User: ' + str(context['user'])
         return context
 
+    def get_object(self):
+        return get_object_or_404(Users, pk=self.request.user.pk)
+
 
 # class TagUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
 class UserUpdateView(LoginRequiredMixin, UpdateView):
     model = Users
     template_name = 'user_update.html'
     # success_url = reverse_lazy('home')
-    # success_url = reverse_lazy('user_detail request.user.id')
     # fields = '__all__'
     # permission_required = 'users_app.change_user'
-
     fields = ['username', 'first_name', 'last_name', 'email', 'image', ]
+
+
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'title of page'
         return context
 
+    def get_object(self):
+        return get_object_or_404(Users, pk=self.request.user.pk)
     # def form_valid(self, form):
     #     if self.request.method == 'POST':
     #         form = self.request.POST, self.request.FILES
     #     if form.is_valid():
     #         form.save()
     #     return super().form_valid(form)
-
-
