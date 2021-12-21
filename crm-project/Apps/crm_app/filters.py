61da0b76cc3e46cc7f43e8110caf71f5d785fcdd
vitosv03 <vitosv03@gmail.com>
2021-12-21 22:50:06 +0200

    remake dropdown filters and sort

--- crm-project/Apps/crm_app/filters.py
+++ crm-project/Apps/crm_app/filters.py
@@ -3,6 +3,7 @@ from urllib import request
 import django_filters
 from django import forms
 from django.contrib.messages.storage import session
+from django_filters.widgets import LinkWidget, SuffixedMultiWidget, CSVWidget, LookupChoiceWidget, QueryArrayWidget
 
 from .models import ClientsInfo, ProjectsList, InterPlaysList
 from .utils import SORT_CHOICES_InterplaysFilter, SORT_CHOICES_ClientsInfoFilter
@@ -34,45 +35,64 @@ class ClientsInfoFilter(django_filters.FilterSet):
 
 class InterplaysFilter(django_filters.FilterSet):
     q_set = InterPlaysList.objects.values_list('project')
+
     project = django_filters.ModelChoiceFilter(
-        label='Project',
+        # label='Project',
         queryset=ProjectsList.objects.filter(pk__in=q_set),
-        widget=forms.Select(attrs={'onchange': "this.form.submit()"}))
+        # widget=forms.Select(attrs={'onchange': "this.form.submit()"})
+    )
+
     client = django_filters.ModelChoiceFilter(
-        label='Client',
+        # label='Client',
         queryset=ClientsInfo.objects.filter(projectslist__pk__in=q_set),
-        widget=forms.Select(attrs={'onchange': "this.form.submit()"}))
-    sort = django_filters.ChoiceFilter(
-        label='Sort by',
-        choices=SORT_CHOICES_InterplaysFilter,
-        method='filter_by_order',
-        widget=forms.Select(attrs={'onchange': "this.form.submit()"}))
+        # widget=forms.Select(attrs={'onchange': "this.form.submit()"})
+    )
 
-    # a = InterPlaysList.objects.all().filter(created_by=1).values_list('created_by')
-    # print('---',a)
-    # created_by = django_filters.ModelChoiceFilter(
-    #     field_name='created_by',
-    #     label='created_by',
-    #     queryset=InterPlaysList.objects.only('created_by'),
-    #     widget=forms.Select(attrs={'onchange': "this.form.submit()"})
-    # )
+    sort = django_filters.OrderingFilter(
+        # label='Sort by',
+        fields=('client',
+                'project',
+                'date_created',
+                ),
+        choices=(
+            ('client', 'client_acs'),
+            ('-client', 'client_desc'),
+            ('project', 'project_acs'),
+            ('-project', 'project_desc'),
+            ('date_created', 'date_created'),
+            ('-date_created', 'created_desc'),
+         ),
+        # field_labels={
+        #     'client', 'client_acs',
+        #     '-client', 'client_desc',
+        #     'project', 'project_acs',
+        #     '-project', 'project_desc',
+        #     'date_created', 'date_created',
+        #     '-date_created', 'created_desc',
+        # },
+    )
 
-    # # print(session.user)
-    @staticmethod
-    def filter_by_order(queryset, name, value):
-        if value == 'client_acs':
-            sorting = 'client'
-        elif value == 'client_desc':
-            sorting = '-client'
-        elif value == 'project_acs':
-            sorting = 'project'
-        elif value == 'project_desc':
-            sorting = '-project'
-        elif value == 'created_acs':
-            sorting = 'date_created'
-        elif value == 'created_desc':
-            sorting = '-date_created'
-        return queryset.order_by(sorting)
+    # sort = django_filters.ChoiceFilter(
+    #     label='Sort by',
+    #     choices=SORT_CHOICES_InterplaysFilter,
+    #     method='filter_by_order',
+    #     widget=forms.Select(attrs={'onchange': "this.form.submit()"}))
+    #
+    # @staticmethod
+    # def filter_by_order(queryset, name, value):
+    #     if value == 'client_acs':
+    #         sorting = 'client'
+    #     elif value == 'client_desc':
+    #         sorting = '-client'
+    #     elif value == 'project_acs':
+    #         sorting = 'project'
+    #     elif value == 'project_desc':
+    #         sorting = '-project'
+    #     elif value == 'created_acs':
+    #         sorting = 'date_created'
+    #     elif value == 'created_desc':
+    #         sorting = '-date_created'
+    #     return queryset.order_by(sorting)
 
     class Meta:
         model = ProjectsList
