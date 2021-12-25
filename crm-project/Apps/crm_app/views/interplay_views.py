8eb948ddc2fde495b7065992e92b5b45482b6e90
vitosv03 <vitosv03@gmail.com>
2021-12-25 23:26:42 +0200

    clean code

--- crm-project/Apps/crm_app/views/interplay_views.py
+++ crm-project/Apps/crm_app/views/interplay_views.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
-from django.shortcuts import get_object_or_404, get_list_or_404
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
