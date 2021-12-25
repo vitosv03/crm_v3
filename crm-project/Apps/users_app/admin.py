9ded880c26624ffaef1da16d37e2acff8e3bad8c
vitosv03 <vitosv03@gmail.com>
2021-12-25 17:47:49 +0200

    add view user's photo in admin panel

--- crm-project/Apps/users_app/admin.py
+++ crm-project/Apps/users_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.safestring import mark_safe
 from django.utils.translation import gettext, gettext_lazy as _
 from .models import Users
 # from .models import Users
@@ -9,7 +10,9 @@ from .models import Users
 
 class UsersAdmin(BaseUserAdmin):
     list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'image', 'date_created',
-                    'date_updated', )
+                    'date_updated', 'get_image',)
+    readonly_fields = ('date_created', 'date_updated', 'get_image',)
+
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
         (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
@@ -18,15 +21,22 @@ class UsersAdmin(BaseUserAdmin):
         }),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
         (_('dates'), {'fields': ('date_created', 'date_updated')}),
-        (_('Profile Photo'), {'fields': ('image', )}),
+        (_('Profile Photo'), {'fields': ('image', 'get_image')}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('username', 'password1', 'password2', 'image'),
+            'fields': ('username', 'password1', 'password2', 'image', ),
         }),
     )
-    readonly_fields = ('date_created', 'date_updated')
+
+    def get_image(self, obj):
+        if obj.image:
+            return mark_safe(f'<img src={obj.image.url} width="100" height="100">')
+
+    get_image.short_description = 'image'
+
+
 
 
 admin.site.register(Users, UsersAdmin)
