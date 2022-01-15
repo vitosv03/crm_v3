ee1de34f075d9903d40b84fc27a4e70e383ee313
vitosv03 <vitosv03@gmail.com>
2022-01-15 17:22:06 +0200

    add modal window (login to CRM) into registration.html

--- crm-project/Apps/users_app/forms.py
+++ crm-project/Apps/users_app/forms.py
@@ -13,21 +13,38 @@ class LoginUserForm(AuthenticationForm):
     username = forms.CharField(label='Login', widget=forms.TextInput())
     password = forms.CharField(label='Password', widget=forms.PasswordInput())
 
-    def get_invalid_login_error(self):
+    # def confirm_login_allowed(self, user):
+    #     """
+    #     Controls whether the given User may log in. This is a policy setting,
+    #     independent of end-user authentication. This default behavior is to
+    #     allow login by active users, and reject login by inactive users.
+    #
+    #     If the given user cannot log in, this method should raise a
+    #     ``ValidationError``.
+    #
+    #     If the given user may log in, this method should return None.
+    #     """
+    #     if not user.is_active:
+    #         raise self.ValidationError(
+    #             self.error_messages['inactive'],
+    #             code='inactive',
+    #         )
+    # def get_invalid_login_error(self):
 
-        User = get_user_model()
-
-        user = User.objects.get(username=self.cleaned_data.get('username'))
-
-        if not user.is_active and user:
-            raise forms.ValidationError(
-                self.error_messages['inactive'],
-                code='inactive', )
-        else:
-            return forms.ValidationError(
-                self.error_messages['invalid_login'],
-                code='invalid_login',
-                params={'username': self.username_field.verbose_name}, )
+        # User = get_user_model()
+        #
+        # user = User.objects.get(username=self.cleaned_data.get('username'))
+        # print(user.exists())
+        # # if not user.is_active and user:
+        # #     raise forms.ValidationError(
+        # #         self.error_messages['inactive'],
+        # #         code='inactive', )
+        # # else:
+        # #     return forms.ValidationError(
+        # #         self.error_messages['invalid_login'],
+        # #         code='invalid_login',
+        # #         # params={'username': self.username_field.verbose_name},
+        # #     )
 
 
 class UserRegisterForm(UserCreationForm):
